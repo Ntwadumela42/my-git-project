50d549f41b1ec841f8742669cc22d77e41c7cc1b
Ntwadumela42 <mcalliater@gmail.com>
2018-04-04 10:34:06 -0600

    Added is_square fxn

---
+++ is_square.py
@@ -0,0 +1,10 @@
+#Quick function to determine if a number is a perfect square.
+
+import math
+def is_square(n):
+    if n <= 0:
+        return False
+    elif match.sqrt(n) == math.sqrt(round(n)):
+        return True
+    else:
+        return False
