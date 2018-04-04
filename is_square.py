91368eb04956df76620a994f49cf16bd5fc617ab
Ntwadumela42 <mcalliater@gmail.com>
2018-04-04 10:47:13 -0600

    Fix the order of operations, added an input and print

--- is_square.py
+++ is_square.py
@@ -1,10 +1,12 @@
 #Quick function to determine if a number is a perfect square.
 
 import math
+n = int(input("Please enter a number: "))
 def is_square(n):
     if n <= 0:
         return False
-    elif match.sqrt(n) == math.sqrt(round(n)):
+    elif math.sqrt(n) == round(math.sqrt(n)):
         return True
     else:
         return False
+print(is_square(n))
