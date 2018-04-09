c28e1eb5de3a62bc35edc35beb995ff410143abe
Ntwadumela42 <mcalliater@gmail.com>
2018-04-09 14:07:32 -0600

    You say goodbye, and I say hello.

--- goodbye.py
+++ goodbye.py
@@ -1,2 +1,10 @@
-print("Goodbye")
+def goodbye_hello():
+    x = input ("Do you say goodbye or hello?")
+    if x == "goodbye":
+        print ('Hello')
+    else:
+        print ('Goodbye')
+
+
+goodbye_hello()
 #You say goodbye, and I say hello.
